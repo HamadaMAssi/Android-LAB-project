7392618e25998ad49c4ddb2fe0ac2f9c6d1f84f6
HamadaAssi <hamada98assi@gmail.com>
2023-07-01 22:21:28 +0300

    ok

--- app/src/main/java/edu/birzeit/android_lab_project/AdminHomeFragment.java
+++ app/src/main/java/edu/birzeit/android_lab_project/AdminHomeFragment.java
@@ -3,24 +3,20 @@ package edu.birzeit.android_lab_project;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdminHomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +24,6 @@ public class AdminHomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AdminHomeFragment newInstance(String param1, String param2) {
         AdminHomeFragment fragment = new AdminHomeFragment();
         Bundle args = new Bundle();
@@ -59,6 +46,63 @@ public class AdminHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_home, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_admin_home, container, false);
+        LinearLayout createNewCourse = view.findViewById(R.id.createNewCourse);
+        LinearLayout editOrDelete = view.findViewById(R.id.editOrDelete);
+        LinearLayout makeAvailable = view.findViewById(R.id.makeAvailable);
+        LinearLayout takeDecisions = view.findViewById(R.id.takeDecisions);
+        LinearLayout viewProfiles = view.findViewById(R.id.viewProfiles);
+        LinearLayout viewOfferingHistory = view.findViewById(R.id.viewOfferingHistory);
+
+        createNewCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new CreateNewCourseFragment()).commit();
+            }
+        });
+
+        editOrDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new EditDeleteCourseFragment()).commit();
+            }
+        });
+        makeAvailable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new MakeCourseAvailableFragment()).commit();
+            }
+        });
+        takeDecisions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new TakeDecisionsFragment()).commit();
+            }
+        });
+        viewProfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new ViewProfilesFragment()).commit();
+            }
+        });
+        viewOfferingHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, new ViewOfferingHistoryFragment()).commit();
+            }
+        });
+
+        return view;    }
 }
